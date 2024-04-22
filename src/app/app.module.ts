bb9b14b2845d9eb393f843ce212d15337cdcda32
mdiasfran <contate.afran@gmail.com>
2024-04-22 18:26:01 -0300

    add funcionalidades

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { PagesModule } from './pages/pages.module';
@@ -13,6 +13,7 @@ import { PagesModule } from './pages/pages.module';
     BrowserModule,
     AppRoutingModule,
     PagesModule,
+    HttpClientModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/shared/poke-list/poke-list.component.ts
+++ src/app/shared/poke-list/poke-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { PokeApiService } from 'src/app/service/poke-api.service';
 
 @Component({
   selector: 'poke-list',
@@ -6,10 +7,28 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./poke-list.component.scss']
 })
 export class PokeListComponent implements OnInit {
+  private setAllPokemons: any
+  public getAllPokemons: any
+  public apiError: boolean = false
 
-  constructor() { }
+  constructor(private pokeApiService: PokeApiService ) { }
 
   ngOnInit(): void {
+    this.pokeApiService.apiListAllPokemons.subscribe(res => {
+      this.setAllPokemons = res.results
+      this.getAllPokemons = this.setAllPokemons
+    },
+  error => {
+    this.apiError = true
+  })
+  }
+
+  public getSearch(value: string) {
+    const filter = this.setAllPokemons.filter( (res: any ) => {
+      return !res.name.indexOf(value.toLowerCase())
+    })
+
+    this.getAllPokemons = filter
   }
 
 }
